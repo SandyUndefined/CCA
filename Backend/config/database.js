8c0a43d444df64cb62cea4c02b2c886f471608a8
Sandy_undefined <08sandysk@gmail.com>
2024-03-04 19:57:38 +0530

    modified database.js file

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,14 +1,39 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017", {
-  dbName: "IITMandi_Project",
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+// mongoose.connect("mongodb://127.0.0.1:27017", {
+//   dbName: "IITMandi_Project",
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// module.exports = db;
+
+const mysql = require("mysql2");
+const config = require("./config.json");
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-module.exports = db;
+// Connect to MySQL
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as id", connection.threadId);
+});
+
+module.exports = pool.promise();
