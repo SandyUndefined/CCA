671d06ab6d5749b33379231b20429a0e6a96eb8e
Sandy_undefined <08sandysk@gmail.com>
2024-03-04 20:38:39 +0530

    modified actuator file

--- Backend/controllers/actuatorControllers.js
+++ Backend/controllers/actuatorControllers.js
@@ -1,40 +1,36 @@
-const actuatorModel = require("../models/actuatorModel");
+const Actuator = require("../models/Actuator"); // Assuming you've created the Actuator model
 
 exports.updateValue = async (req, res) => {
-    try {
-      const { plantName } = req.params;
-      const { sensors } = req.body;
-  
-      const filter = { "plants.name": plantName };
-      const updatedValue = await actuatorModel.findOne(filter);
-  
-      if (!updatedValue) {
-        // Plant doesn't exist, create a new document
-        const newPlant = { name: plantName, sensors };
-        await actuatorModel.create({ plants: [newPlant] });
-        return res.json(newPlant);
-      }
-  
-      // Update the existing plant's sensors
-      const updatedPlant = updatedValue.plants.find((plant) => plant.name === plantName);
-      updatedPlant.sensors = sensors;
-      await updatedValue.save();
-  
-      console.log(updatedValue);
-      res.json(updatedValue);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json(err);
+  try {
+    const { plantName } = req.params;
+    const { sensors } = req.body;
+
+    // Check if the plant exists
+    let actuator = await Actuator.findOne({ where: { plantName } });
+
+    if (!actuator) {
+      // If the plant doesn't exist, create a new one
+      actuator = await Actuator.create({ plantName, ...sensors });
+      return res.json(actuator);
     }
-  };
-  
+
+    // If the plant exists, update its sensors
+    await Actuator.update(sensors, { where: { plantName } });
+    actuator = await Actuator.findOne({ where: { plantName } });
+
+    res.json(actuator);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+};
 
 exports.getValue = async (req, res) => {
   try {
     const { plantName } = req.params;
-    const query = plantName ? { "plants.name": plantName } : {};
+    const query = plantName ? { where: { plantName } } : {};
 
-    const values = await actuatorModel.find(query);
+    const values = await Actuator.findAll(query);
     res.json(values);
   } catch (err) {
     console.error(err);
